4a934fc5f45a3bf6dfa292450b5344f14ea1edea
Nelson Maldonado <play2nino@gmail.com>
2021-10-25 18:20:35 -0500

    add button and subtract button working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react"
 
 class App extends React.Component {
-  constructor () {
-    super();
-    this.state =[
-      {task="Wash Car", completed=false, id=Date.now()}
-  ]
+  constructor() {
+    super()
+    this.state = [{ task: "Wash Car", completed: false, id: Date.now() }]
   }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  handleAdd = () => {
+    console.log("Clicked add button")
+  }
+  handleSubtract = () => {
+    console.log("Subtract button clicked")
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <button onClick={this.handleAdd}>Add</button>
+        <button onClick={this.handleSubtract}>Subtract</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
